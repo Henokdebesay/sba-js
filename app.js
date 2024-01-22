6c8c4615ce6b54efa0dfecdeedb253a169ed93a2
Henok Debesay <henokdebesay@gmail.com>
2024-01-22 13:41:29 -0800

    just console.log some variable for test purposes

--- app.js
+++ app.js
@@ -4,4 +4,49 @@ const CourseInfo = {
     name: "Introduction to JavaScript"
   };
   
+//   function getLearnerData(course, ag, submissions) {
+//     // here, we would process this data to achieve the desired result.
+
+
+//     const result = [
+//       {
+//         id: 125,
+//         avg: 0.985, // (47 + 150) / (50 + 150)
+//         1: 0.94, // 47 / 50
+//         2: 1.0 // 150 / 150
+//       },
+//       {
+//         id: 132,
+//         avg: 0.82, // (39 + 125) / (50 + 150)
+//         1: 0.78, // 39 / 50
+//         2: 0.833 // late: (140 - 15) / 150
+//       }
+//     ];
+  
+//     return result;
+//   }
+  
+//   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+const LearnerSubmissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    }
+]
+
+
+
+
   
